104867863a7c1614c15a38458d7a0e88f569eef3
Grzegorz Popiel <grzegorz.popiel@yahoo.com>
2021-02-07 23:24:35 +0100

    Update SwiperComponent

--- src/components/Swiper/SwiperComponent.vue
+++ src/components/Swiper/SwiperComponent.vue
@@ -1,63 +1,53 @@
 <template>
   <swiper
     :slides-per-view="1"
-    :pagination="{ 
+    :pagination="{
       clickable: true,
     }"
     @swiper="onSwiper"
     @slideChange="onSlideChange"
     :autoplay="{
-        delay: 1000,
+      delay: 1000,
     }"
+    :speed="800"
     effect="fade"
+    :loop="true"
   >
-    <swiper-slide>
-      <div class="slider-wrapper" style="background: red;"></div>
-    </swiper-slide>
-    <swiper-slide>
-      <div class="slider-wrapper" style="background: green;"></div>
-    </swiper-slide>
-    <swiper-slide>
-      <div class="slider-wrapper" style="background: white;"></div>
-    </swiper-slide>
-    
+    <slot> </slot>
   </swiper>
 </template>
 <script>
-  // import Swiper core and required modules
-  import SwiperCore, { Navigation, Pagination, Scrollbar, A11y, Autoplay, EffectFade } from 'swiper';
+// import Swiper core and required modules
+import SwiperCore, {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  Autoplay,
+  EffectFade,
+} from "swiper";
 
-  // Import Swiper Vue.js components
-  import { Swiper, SwiperSlide } from 'swiper/vue';
+// Import Swiper Vue.js components
+import { Swiper } from "swiper/vue";
 
-  // Import Swiper styles
-  import 'swiper/swiper.scss';
-  import 'swiper/components/navigation/navigation.scss';
-  import 'swiper/components/pagination/pagination.scss';
-  import 'swiper/components/scrollbar/scrollbar.scss';
+// Import Swiper styles
+import "swiper/swiper-bundle.css";
+import "swiper/swiper.scss";
+import "swiper/components/navigation/navigation.scss";
+import "swiper/components/pagination/pagination.scss";
+import "swiper/components/scrollbar/scrollbar.scss";
 
-  // install Swiper modules
-  SwiperCore.use([Navigation, Pagination, Scrollbar, A11y, Autoplay, EffectFade]);
+// install Swiper modules
+SwiperCore.use([Navigation, Pagination, Scrollbar, Autoplay, EffectFade]);
 
-  // Import Swiper styles
-  export default {
-    components: {
-      Swiper,
-      SwiperSlide,
-    },
-    data() {
-      return {
-      }
-    },
-    props: {
-    },
-    methods: {
-      onSwiper(swiper) {
-        console.log(swiper);
-      },
-      onSlideChange() {
-        console.log('slide change');
-      },
-    },
-  };
+// Import Swiper styles
+export default {
+  components: {
+    Swiper,
+  },
+  data() {
+    return {};
+  },
+  props: {},
+  methods: {},
+};
 </script>
