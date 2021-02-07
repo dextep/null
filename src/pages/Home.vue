9bd39dd0d8f1cf7e0812f730f27ed962b23cc111
Grzegorz Popiel <grzegorz.popiel@yahoo.com>
2021-02-07 23:25:05 +0100

    Add swiper on main page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,15 +1,57 @@
 <template>
   <div class="wrapper p-0">
-      <swiper-component></swiper-component>
+      <swiper-component>
+          <template v-for="(product, index) in products" :key="index">
+            <swiper-slide>
+              <img
+                v-if="product.img"
+                :src="product.img.url"
+                :alt="product.img.name"
+                loading="lazy"
+              />
+            </swiper-slide>
+          </template>
+      </swiper-component>
   </div>
 </template>
 
 <script>
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/storage";
 import SwiperComponent from "@/components/Swiper/SwiperComponent.vue";
+  import { SwiperSlide } from 'swiper/vue';
 
 export default {
   components: {
     SwiperComponent,
+    SwiperSlide,
+  },
+  data() {
+    return {
+      products: {},
+    };
+  },
+  methods: {
+    async useEffect() {
+      const productsRef = firebase.firestore().collection("main_slider");
+      const snapshot = await productsRef.get();
+
+      if (snapshot.empty) {
+        console.log("No matching products.");
+        return;
+      }
+
+      let list = [];
+      snapshot.forEach((doc) => {
+        console.log(doc.data());
+        list.push(doc.data());
+      });
+      this.products = list;
+    },
+  },
+  mounted() {
+    this.useEffect();
   },
 };
 </script>
